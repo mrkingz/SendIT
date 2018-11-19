44e09f60d3f03b14d9cc4f16423f387cce89be2a
Kingsley Frank-Demesi <frankdemesikingsley@gmail.com>
2018-11-20 00:20:53 +0100

    Setup databse configuration

---
+++ server/configs/index.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const testConfig = {
+  user: process.env.DB_USERNAME_TEST,
+  database: process.env.DB_NAME_TEST,
+  password: process.env.DB_PASSWORD_TEST,
+  port: process.env.DB_PORT_TEST,
+  host: process.env.DB_HOST_TEST,
+};
+
+const devConfig = {
+  user: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+};
+
+export {
+   devConfig,
+   testConfig
+};
