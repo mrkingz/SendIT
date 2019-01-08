927d04bbcf8c7148fdee7d2302c18670d0cf7e9b
Kingsley Frank-Demesi <kingsleyfrankdemesi@gmail.com>
2019-01-08 06:44:46 +0100

    Update parcelController.js

--- server/controllers/parcelController.js
+++ server/controllers/parcelController.js
@@ -1,5 +1,5 @@
-import ParcelService from '../services/ParcelService';
-import Controller from './controller';
+import ParcelService from '../services/parcelService';
+import Controller from './Controller';
 
 /**
  * @export
